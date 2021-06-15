7dee7fb0ff2873916d5ea23156545f3a58514218
Roozbeh Sayadi <roozbehsayadi@gmail.com>
2021-06-15 09:55:14 -0700

    Still no idea why this doesn't work. Commited minor changes though.

--- blur.c
+++ blur.c
@@ -14,15 +14,14 @@ void main () {
 	volatile __xdata unsigned char *shared = 
 		(volatile __xdata unsigned char *) 0x4000;
 	
-	
 	unsigned i, j;
-	for ( i = 0; i < 100; i++ ) 
-		for ( j = 0; j < 100; j++ ) 
+	for ( i = 0; i < 10; i++ ) 
+		for ( j = 0; j < 10; j++ ) 
 			shared[i*10+j] = 50;
 	
 	P0 = INS_START;
 	
-	while ( P1 );
+	while ( !P1 );
 	
 	terminate();
 
