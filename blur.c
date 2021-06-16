b167ad82aa26fd61a7d98bbe6b47ce93e60ad656
Roozbeh Sayadi <roozbehsayadi@gmail.com>
2021-06-15 20:43:30 -0700

    Works! ^_^

--- blur.c
+++ blur.c
@@ -1,6 +1,8 @@
 
 #include <8051.h>
 
+#include <stdlib.h>
+
 enum {
 	INS_IDLE, INS_START
 };
@@ -13,15 +15,21 @@ void main () {
 
 	volatile __xdata unsigned char *shared = 
 		(volatile __xdata unsigned char *) 0x4000;
+		
+	unsigned image_counter;
+	for ( image_counter = 0; image_counter < 8; image_counter++ ) {
 	
-	unsigned i, j;
-	for ( i = 0; i < 10; i++ ) 
-		for ( j = 0; j < 10; j++ ) 
-			shared[i*10+j] = 50;
+		unsigned i, j;
+		for ( i = 0; i < 10; i++ ) 
+			for ( j = 0; j < 10; j++ ) 
+				shared[i*10+j] = rand() % 256;
+		
+		P0 = INS_START;
+		P0 = INS_IDLE;
 	
-	P0 = INS_START;
+		while ( P1 == 0 );
 	
-	while ( !P1 );
+	}
 	
 	terminate();
 
